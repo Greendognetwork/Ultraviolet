833a1ac92f9d5e0153d34bb421cb39d1bac5fde5
coolgamerkid332 <116192687+coolgamerkid332@users.noreply.github.com>
2023-07-19 16:38:01 -0500

    goofy guy

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "/bare/",
+  bare: "https://latest.n-e-t.name/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
