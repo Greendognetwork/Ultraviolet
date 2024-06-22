188715fd42ce79a9875b24ac103589e88ed3e708
GDG-network <162386566+gdg-network@users.noreply.github.com>
2024-06-22 19:12:17 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "https://tribe.norushcharge.com/bare/",
+  bare: "https://uv.holyubofficial.net/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
